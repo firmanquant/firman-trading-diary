27fe3129f80a0485e687b523d73a42265c5894cf
firmanquant <firman_zulhansyah@yahoo.com>
2025-05-28 10:16:29 +0700

    Fix duplicate import of index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import TradingDiary from './TradingDiary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
