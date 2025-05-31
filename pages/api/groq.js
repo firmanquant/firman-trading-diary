77d7dd8122a398ad85965f918c7dee5f403f27c5
firmanquant <firman_zulhansyah@yahoo.com>
2025-05-31 08:17:18 +0700

    Update groq.js

--- pages/api/groq.js
+++ pages/api/groq.js
@@ -1,1 +1,6 @@
-
+export async function getGroqAnalysis(symbol) {
+  // Simulasi response dummy
+  return {
+    response: `Analisis teknikal untuk ${symbol}: Tren naik, volume meningkat, peluang breakout.`
+  };
+}
