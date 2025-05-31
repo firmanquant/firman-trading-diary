8c93b33bc30b10cb785f9be16c57e949e2e86bf3
firmanquant <firman_zulhansyah@yahoo.com>
2025-05-31 14:29:26 +0700

    Update TradingDiary.js

--- src/TradingDiary.js
+++ src/TradingDiary.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { fetchData, fetchSignal } from "./api";
+import { fetchData } from "./lib/groq";
+import { fetchSignal } from "./lib/signal";
 import TradingViewWidget from "./TradingViewWidget";
 import SignalDashboard from "./SignalDashboard";
 import TradeTable from "./TradeTable";
